dc2c94245bad1fd54e70145039beb2442bb86b10
Wasim Iqbal <wasimgulrangzai@GMAIL.COM>
2026-06-16 20:23:28 +0500

    Fix import error for archive in runner

--- experiments/run_psafe_v1_experiments.py
+++ experiments/run_psafe_v1_experiments.py
@@ -1,8 +1,9 @@
 import os
 import sys
 
-# Add src to python path to allow importing psafe
+# Add src and root to python path to allow importing psafe and archive
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import yaml
 import argparse
